104f92aab121be8e9b5b5b79a9dd4f82bef35480
Micah Shallom <micahshallom@gmail.com>
2022-08-25 13:32:43 +0100

    added code update to vcs

--- script.groovy
+++ script.groovy
@@ -25,7 +25,8 @@ def deployApp() {
 
 def commitVersion(){
     echo "Commiting new software version to github"
-    withCredentials([usernamePassword(credentialsId:'github-credentials', passwordVariable: 'PASSWD', usernameVariable: 'USER')]){
+    // withCredentials([usernamePassword(credentialsId:'github-credentials', passwordVariable: 'PASSWD', usernameVariable: 'USER')]){
+    withCredentials([gitUsernamePassword(credentialsId: 'github-credentials', gitToolName: 'git-tool')]){
             sh "git config --global user.email 'micahshallom@gmail.com'"
             sh "git config --global user.name 'Shallom Micah'"
 
@@ -33,11 +34,26 @@ def commitVersion(){
             sh 'git branch'
             sh 'git config --list'
 
-            sh 'git remote set-url origin https://${USER}:${PASSWD}@github.com/Micah-Shallom/java-maven-app.git'
+            // sh 'git remote set-url origin https://${USER}:${PASSWD}@github.com/Micah-Shallom/java-maven-app.git'
+            sh 'git remote set-url origin git@github.com:Micah-Shallom/java-maven-app.git'
             sh "git add ."
             sh "git commit -m 'ci: version bump'"
             sh "git push origin HEAD:jenkins-versioning"
         }
+    // withCredentials([gitUsernamePassword(credentialsId: 'github-credentials', gitToolName: 'git-tool')]){
+    //         sh "git config --global user.email 'micahshallom@gmail.com'"
+    //         sh "git config --global user.name 'Shallom Micah'"
+
+    //         sh 'git status'
+    //         sh 'git branch'
+    //         sh 'git config --list'
+
+    //         sh 'git remote set-url origin git@github.com:Micah-Shallom/java-maven-app.git'
+    //         sh "git add ."
+    //         sh "git commit -m 'ci: version bump'"
+    //         sh "git push origin HEAD:jenkins-versioning
+    //         // sh 'git remote set-url origin https://${USER}:${PASSWD}@github.com/Micah-Shallom/java-maven-app.git'
+    //     }
 }
 
 return this
