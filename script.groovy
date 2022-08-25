e57fcf4fd31cb0e30c53e09a7dfeff1278c5ad31
Micah Shallom <micahshallom@gmail.com>
2022-08-25 11:59:31 +0100

    added code update to vcs

--- script.groovy
+++ script.groovy
@@ -38,7 +38,7 @@ def commitVersion(){
             sh 'git branch'
             sh 'git config --list'
 
-            sh "git remote set-url origin https://${USER}:${PASSWD}@https://github.com/Micah-Shallom/java-maven-app.git"
+            sh "git remote set-url origin https://${USER}:${PASSWD}@github.com/Micah-Shallom/java-maven-app.git"
             sh "git add ."
             sh "git commit -m 'ci: version bump'"
             sh "git push origin HEAD:jenkins-versioning"
