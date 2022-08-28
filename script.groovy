643eb0506aa364be430720659e7504860fd46373
Micah Shallom <micahshallom@gmail.com>
2022-08-28 19:38:31 +0100

    fixed

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 def deployImage(String IMAGE_NAME) {
     echo 'deploying the application...'
-    def dockerCMD = "docker run -d -p 3000:80 ${IMAGE_NAME}"
+    def dockerCMD = "docker run -d -p 4000:80 ${IMAGE_NAME}"
     sshagent(['ec2-server-key']) {
         sh "ssh -o StrictHostKeyChecking=no ec2-user@34.228.40.92 ${dockerCMD}"
     }
